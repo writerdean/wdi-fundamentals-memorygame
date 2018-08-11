87167dd21f0befd1b75672ab1acfd4f19311174d
writerdean <tracyblackadder@yahoo.com>
2018-08-11 15:16:59 +1000

    created game logic again

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,28 @@
+
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log('User flipped ' + cardOne);
-console.log('User flipped ' + cardTwo);
-
-console.log(cardsInPlay.length);
-
-if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-    	alert('You found a match!');
-    } else {
-    	alert('Sorry, try again!');
-    }
+var checkForMatch = function () {
+		  if (cardsInPlay[0] === cardsInPlay[1]) {
+		  	console.log("You found a match");
+		  //alert("You found a match!");
+	  } else {
+		  alert("Sorry, try again.");
+	  }
 }
+
+var flipCard = function (cardId) {
+
+  var card = cards[cardId];
+  cardsInPlay.push(card);
+
+  console.log('User flipped ' + card);
+
+  if (cardsInPlay.length === 2) {
+    checkForMatch;
+  }
+}
+
+flipCard(0);
+flipCard(1);
+
